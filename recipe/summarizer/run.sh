8c0f80ca414391e7257ab996aea24d60ff5182ea
micaebe <eberhard.michael08@googlemail.com>
2025-06-07 16:36:47 +0200

    Config

--- recipe/summarizer/run.sh
+++ recipe/summarizer/run.sh
@@ -19,7 +19,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.path=Qwen/Qwen3-0.6B \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=4 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=2 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
