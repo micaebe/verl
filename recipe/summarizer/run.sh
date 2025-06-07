0803bd3def08a996544d5cd5e0e4e021214678de
micaebe <eberhard.michael08@googlemail.com>
2025-06-07 14:13:10 +0200

    Config

--- recipe/summarizer/run.sh
+++ recipe/summarizer/run.sh
@@ -12,8 +12,8 @@ python3 -m verl.trainer.main_ppo \
     data.train_files=$DATA_PATH/train.parquet \
     data.val_files=$DATA_PATH/test.parquet \
     data.train_batch_size=16 \
-    data.max_prompt_length=1536 \
-    data.max_response_length=3072 \
+    data.max_prompt_length=2560 \
+    data.max_response_length=3584 \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
     actor_rollout_ref.model.path=Qwen/Qwen3-0.6B \
